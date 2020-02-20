c3d83e05fc59d8d8d4f5409951f27c17eaa5805b
mmlumba <mmlumba@gmail.com>
2020-02-20 10:59:29 -0500

    why not add a real test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,8 +2,28 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { MockedProvider } from '@apollo/client/testing';
+import { GET_COMMENTS } from './graphql'
+
+const mocks = [
+  {
+    request: {
+      query: GET_COMMENTS,
+    },
+    result: {
+      data: {
+        comments: { id: '1', name: 'Buck', email: 'buck@wild.com', body: 'Howdy', createdAt: '1582164534965' },
+      },
+    },
+  },
+];
+
+test('shows app', async () => {
+  const component = render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <App />
+    </MockedProvider>
+  );
+  const title = component.getByText(/Guestbook/i);
+  expect(title).toBeInTheDocument();
 });
